421aedf085df7d4d0003f964c64f0a1b64440302
trongkha <trongkha2112@gmail.com>
2018-01-02 14:41:09 +0700

    chapter 10

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       log_in @user
       checked = params[:session][:remember_me] == Settings.checked_rememberme
       checked ? remember(@user) : forget(@user)
-      redirect_to @user
+      redirect_back_or @user
     else
       flash.now[:danger] = t "flash.invalid_login"
       render :new
