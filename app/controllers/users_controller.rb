99bfbc0759c5ecd227b223d4890986871b593f43
trongkha <trongkha2112@gmail.com>
2018-01-03 08:53:37 +0700

    chapter 11

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :admin_user, only: :destroy
 
   def index
-    @users = User.paginate page: params[:page]
+    @users = User.where(activated: true).paginate page: params[:page]
   end
 
   def show
@@ -21,9 +21,9 @@ class UsersController < ApplicationController
   def create
     @user = User.new user_params
     if @user.save
-      log_in @user
+      @user.send_activation_email
       flash[:success] = t "flash.success"
-      redirect_to @user
+      redirect_to root_url
     else
       render :new
     end
